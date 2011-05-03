7cbaf58ae6d8e500f6e134c6b58abced69535e9d
johannes-b <johannes.br@gmx.net>
2011-05-03 14:08:55 +0200

    <Tag 1> Clemens: Codestruktur von DomainObject.java geändert

--- SortedSetExample/SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -1,22 +1,19 @@
 package at.jku.ce;
 
-
-
-public String id;
-
-
-
 public class DomainObject {
+
+	public String id;
 
 	public DomainObject() {
 		super();
 	}
 
+	public DomainObject(String id) {
+  		super();
+  		this.id = id;
+	}
 }
 
 
 
-public DomainObject(String id) {
-  super();
-  this.id = id;
-}
+
