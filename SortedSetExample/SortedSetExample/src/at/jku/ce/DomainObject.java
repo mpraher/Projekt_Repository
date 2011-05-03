6c1577a703db2fa65d360ee1de2c860b5d60e519
johannes-b <johannes.br@gmx.net>
2011-05-03 15:32:47 +0200

    <Tag 10> Clemens: Comparable Interface in DomainObject.java hinzugefügt.

--- SortedSetExample/SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -1,6 +1,6 @@
 package at.jku.ce;
 
-public class DomainObject {
+public class DomainObject implements Comparable<DomainObject> {
 
 	private String uuid=UUID.randomUUID().toString();
 	private String name;
@@ -46,20 +46,27 @@ public class DomainObject {
 	}
 
 	public boolean equals(Object obj) {
-   		if (!(obj instanceof DomainObject)) {
+   		
+		if (!(obj instanceof DomainObject)) {
       			return false;
    		}
-   	DomainObject domainObj = (DomainObject) obj;
-   	return uuid.equals(domainObj.getUuid());
+
+   		DomainObject domainObj = (DomainObject) obj;
+   		return uuid.equals(domainObj.getUuid());
 	}
 
 	public int hashCode() {
    		if (uuid != null) {
       			return uuid.hashCode();
    		} else {
-      		return super.hashCode();
-   	}
-}
+      			return super.hashCode();
+   		}
+	}
 
+	public int compareTo(DomainObject o) {
+   		DomainObject domainObj = (DomainObject) o;
+   		int domObjeComp = name.compareTo(domainObj.getName());
 
+  		return ((domObjeComp == 0) ? uuid.compareTo(domainObj.getUuid()) : domObjeComp);
+	}
 }
