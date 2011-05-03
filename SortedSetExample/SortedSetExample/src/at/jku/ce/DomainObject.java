ad120a7258527290c11d3c1c52b18b2904a4e41e
karrer <damakozenymholinkami@.(none)>
2011-05-03 14:03:02 +0200

    <Tag 1> Arthur: Attribut id und weiteren Konstruktor hinzugefuegt

--- SortedSetExample/SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -1,9 +1,22 @@
 package at.jku.ce;
 
+
+
+public String id;
+
+
+
 public class DomainObject {
 
 	public DomainObject() {
 		super();
 	}
 
+}
+
+
+
+public DomainObject(String id) {
+  super();
+  this.id = id;
 }
